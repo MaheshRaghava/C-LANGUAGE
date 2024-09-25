69ff3d43597fb9a7bf2d455171c0716f00492b07
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2024-09-25 16:20:22 +0530

    Create Reverse of a number.c

---
+++ Reverse of a number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+int n, rem, num=0;
+printf ("enter the number") ;
+scanf("%d" , &n);
+while (n!=0){
+ram=n%10;  
+num=num*10+rem;  
+n=n/10;}
+printf("The reverse of given number is :%d", num);} 
+      
